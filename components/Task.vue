0eb94c0be7a4a903efc22c4dac155dc2e02dbc84
rozhan sheikholmoluki <rozhansheikholmoluki@gmail.com>
2022-04-07 22:14:27 +0430

    with update to do

--- components/Task.vue
+++ components/Task.vue
@@ -1,26 +1,59 @@
 <template>
-    <div :class="`task ${task.done ? 'is-complete' : ''}`">
-        <div class="content">
-            {{task.content}}
-        </div>
-
-        <div class="buttons">
-            <button @click="toggleDone">{{ task.done ? 'Undo' : 'Done'}}</button>
-            <button @click="removeTask" class="delete"> delete</button>
-        </div>
+  <div :class="`task ${task.done ? 'is-complete' : ''}`">
+    <div v-if="!editing" class="content">
+      {{ task.content }}
     </div>
+
+    <div v-else>
+      <input v-bind:value="todoText" @change="todoTextChange" type="text" />
+    </div>
+
+    <div class="buttons">
+      <button @click="toggleDone">{{ task.done ? "Undo" : "Done" }}</button>
+      <button @click="editTask()" class="btn btn-primary mx-2">
+        {{ editing ? "Update" : "Edit" }}
+      </button>
+      <button @click="removeTask" class="delete">delete</button>
+    </div>
+  </div>
 </template>
 
 <script>
+import { mapActions } from "vuex";
+
 export default {
-    props: ['task'],
-    methods: {
-        toggleDone() {
-            this.$store.commit('TOGGLE_TASK', this.task)
-        },
-        removeTask() {
-            this.$store.commit('REMOVE_TASK', this.task)
-        }
-    }
-}
+  props: ["task"],
+  data() {
+    return {
+      editing: false,
+      todoText: "",
+    };
+  },
+  methods: {
+    toggleDone() {
+      this.$store.commit("TOGGLE_TASK", this.task);
+    },
+    removeTask() {
+      this.$store.commit("REMOVE_TASK", this.task);
+    },
+    editTask() {
+      this.editing = this.editing == true ? false : true;
+      this.todoText = this.task.content;
+      this.$store.commit("EDIT_TODO", this.task);
+    },
+    ...mapActions(["updateTodo", "changeCompleted"]),
+    todoTextChange(e) {
+      this.todoText = e.target.value;
+    },
+    updateTodoI(task) {
+      this.editing = this.editing == true ? false : true;
+      if (this.editing) {
+        this.todoText = task.content;
+        this.updateTodo(task);
+      } else {
+        task.content = this.todoText;
+      }
+    },
+  },
+};
 </script>
--- store/index.js
+++ store/index.js
@@ -1,16 +1,36 @@
 export const state = () => ({
-    tasks: []
+    tasks: [],
 })
 
 export const mutations = {
     ADD_TASK(state, task) {
-        state.tasks = [{ content: task, done: false }, ...state.tasks]
+        state.tasks = [{
+            id: String(Math.floor(Math.random() * 999999999)),
+            content: task,
+            done: false
+        }, ...state.tasks]
     },
     REMOVE_TASK(state, task) {
-        state.tasks.splice(state.tasks.indexOf(), 1)
+        state.tasks = state.tasks.filter(o => o.id !== task.id)
     },
     TOGGLE_TASK(state, task) {
         task.done = !task.done
     },
-    EDIT_TODO(state, task) {}
+    EDIT_TODO(state, task) {
+        state.tasks.filter(o => {
+            o.content = task.content
+        })
+    },
+    update_todo(state, task) {
+        let index = state.tasks.findIndex((t) => t.id == task.id);
+        if (index != -1) {
+            state.tasks[index] = task;
+        }
+    },
+}
+
+export const actions = {
+    updateTodo({ commit }, task) {
+        commit("update_todo", task);
+    },
 }
