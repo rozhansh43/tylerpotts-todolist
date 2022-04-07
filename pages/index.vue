4cdfdc4e10432ed502bb2f010a3dcc92233b3359
rozhan sheikholmoluki <rozhansheikholmoluki@gmail.com>
2022-04-07 20:13:39 +0430

    init files

---
+++ pages/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <main>
+    <h1>task board</h1>
+
+    <div class="create-new">
+      <input type="text" v-model="newTask" @keypress.enter="addTask">
+      <button @click="addTask">add task</button>
+    </div>
+
+    <div class="tasks">
+      <Task 
+        v-for="(task,i) in $store.state.tasks"
+        :key="i"
+        :task="task"
+      />
+        {{ $store.state.tasks}}
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  data () {
+    return {
+      newTask: ""
+    }
+  },
+  methods: {
+    addTask() {
+      if(this.newTask) {
+        this.$store.commit('ADD_TASK', this.newTask)
+        this.newTask = ""
+      }
+    }
+  }
+}
+</script>
