f319d52e6871add477c222af5d71fa57421d91d2
rozhan sheikholmoluki <rozhansheikholmoluki@gmail.com>
2022-04-08 22:40:59 +0430

    api record concating new array fixed

--- pages/index.vue
+++ pages/index.vue
@@ -3,63 +3,71 @@
     <h1>task board</h1>
 
     <div class="create-new">
-      <input type="text" v-model="title" @keypress.enter="addTask">
+      <input type="text" v-model="title" @keypress.enter="addTask" />
+      <br />
+      <input type="text" v-model="type" @keypress.enter="addTask" />
+      <br />
 
-      <input type="text" v-model="type" @keypress.enter="addTask">
+      <input type="text" v-model="value" @keypress.enter="addTask" />
+      <br />
 
-      <input type="text" v-model="value" @keypress.enter="addTask">
-      
       <button @click="addTask">add task</button>
     </div>
 
     <div class="tasks">
-      <Task 
-        v-for="(task,i) in $store.state.tasks"
-        :key="i"
-        :task="task"
-      />
+      <Task v-for="(task, i) in $store.state.tasks" :key="i" :task="task" />
     </div>
 
     <div class="create-new">
       <div>
-        <h1>
-          data:
-        </h1>
+        <h1>data:</h1>
 
         <p>
-         {{ $store.state.tasks}}
+          {{ $store.state.tasks }}
         </p>
       </div>
-      <button>
-        Send data to api
-      </button>
+      <button>Send data to api</button>
     </div>
   </main>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage',
-  data () {
+  name: "IndexPage",
+  data() {
     return {
       title: "",
       type: "",
       value: "",
-    }
+      apiTask: [
+        {
+          id: "996801213",
+          title: "llllllll",
+          type: "kkkkkkkk",
+          value: "iiiiiii",
+          done: false,
+        },
+        { id: "104487746", title: "hhhhhhh", type: "uuuuuuuu", done: false },
+        { id: "855403452", title: "mmmmm", done: false },
+      ],
+    };
   },
   methods: {
     addTask() {
-      if(this.title, this.type,this.value) {
-        this.$store.commit('ADD_TASK', {
+      if ((this.title, this.type, this.value)) {
+        this.$store.commit("ADD_TASK", {
           title: this.title,
           type: this.type,
-          value: this.value
-        })
-        this.title = ""
-        this.type= ""
-        this.value = ""
+          value: this.value,
+        });
+        this.title = "";
+        this.type = "";
+        this.value = "";
       }
-    }
-  }
-}
+    },
+  },
+  mounted() {
+    this.$store.commit("SET_API_TASKS", this.apiTask);
+  },
+};
 </script>
--- store/index.js
+++ store/index.js
@@ -1,31 +1,30 @@
 export const state = () => ({
     tasks: [],
-    apiTasks: [
-        { "id": "996801213", "title": "llllllll", "type": "kkkkkkkk", "value": "iiiiiii", "done": false },
-        { "id": "104487746", "title": "hhhhhhh", "type": "uuuuuuuu", "done": false },
-        { "id": "855403452", "title": "mmmmm", "done": false }
-    ]
-})
+    apiTasks: []
+});
 
 export const mutations = {
     ADD_TASK(state, task) {
-        state.tasks = [{
+        state.tasks.unshift({
             id: String(Math.floor(Math.random() * 999999999)),
             title: task.title,
             type: task.type,
             value: task.value,
             done: false
-        }, ...state.apiTasks]
+        })
+    },
+    SET_API_TASKS(state, apiTasks) {
+        state.tasks = apiTasks;
     },
     REMOVE_TASK(state, task) {
-        state.tasks = state.tasks.filter(o => o.id !== task.id)
+        state.tasks = state.tasks.filter((o) => o.id !== task.id);
     },
     TOGGLE_TASK(state, task) {
-        task.done = !task.done
+        task.done = !task.done;
     },
     EDIT_TODO(state, task) {
-        state.tasks.filter(o => {
-            o.content = task.content
-        })
+        state.tasks.filter((o) => {
+            o.content = task.content;
+        });
     }
-}
+};
